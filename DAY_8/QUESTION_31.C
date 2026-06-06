7ee7de23e78c5f5175b9c4caf61068996a13b3fa
ritesh-203 <rriteshsingh1601@gmail.com>
2026-06-06 12:03:07 +0530

    summer assignement

---
+++ DAY_8/QUESTION_31.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j < i; j++) {
+            printf("%c", 'A' + j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
