2aea48f25a4e4b4c4f198874b16bdf002c4cbc9e
ritesh-203 <rriteshsingh1601@gmail.com>
2026-06-10 18:53:35 +0530

    summer assignement

---
+++ DAY_10/QUESTION_40.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, n = 5;
+    char ch;
+
+    for(i = 1; i <= n; i++) {
+
+        // Print spaces
+        for(j = 1; j <= n - i; j++)
+            printf(" ");
+
+        // Print ascending characters
+        for(j = 1; j <= i; j++)
+            printf("%c", 'A' + j - 1);
+
+        // Print descending characters
+        for(j = i - 1; j >= 1; j--)
+            printf("%c", 'A' + j - 1);
+
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ DAY_12/QUESTION_46.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int armstrong(int n)
+{
+    int sum = 0, rem, temp = n;
+
+    while(temp != 0)
+    {
+        rem = temp % 10;
+        sum = sum + rem * rem * rem;
+        temp = temp / 10;
+    }
+
+    if(sum == n)
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(armstrong(num))
+        printf("Armstrong Number");
+    else
+        printf("Not Armstrong Number");
+
+    return 0;
+}
